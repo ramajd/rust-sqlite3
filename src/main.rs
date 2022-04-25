ce3d0ef920e1f895b770b71302639c55d5e6de6a
Reza Alizadeh Majd <r.a.majd@gmail.com>
2022-04-25 16:32:08 +0430

    initial implementation of repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,75 @@
-fn main() {
-    println!("Hello, world!");
+extern crate clap;
+mod repl;
+
+use clap::{crate_version, Command};
+use repl::{get_config, REPLHelper};
+
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
+fn main() -> rustyline::Result<()> {
+    env_logger::init();
+
+    let _matches = Command::new("Rust-SQLite3")
+        .version("0.0.1")
+        .author("Reza Alizadeh Majd <r.a.majd@gmail.com>")
+        .about("SQLite3 DB engine - re-implemented by Rust")
+        .get_matches();
+
+    let config = get_config();
+    let helper = REPLHelper::new();
+
+    let mut repl = Editor::with_config(config);
+    repl.set_helper(Some(helper));
+
+    if repl.load_history(".history.tmp").is_err() {
+        println!("No previous history.");
+    }
+
+    let mut count = 1;
+    loop {
+        if count == 1 {
+            // TODO: Get info about the application name and version dynamically
+            println!(
+                "{}{}{}{}",
+                format!("Rust-SQLite3 - {}\n", crate_version!()),
+                "Enter .exit to quit.\n",
+                "Enter .help for usage hints.\n",
+                "Connected to a transient in-memory database."
+            );
+        }
+        let p = format!("rust-sqlite3 | {}> ", count);
+        repl.helper_mut() // our helper
+            .expect("No helper found")
+            .colored_prompt = format!("\x1b[1;32m{}\x1b[0m", p);
+
+        let readline = repl.readline(&p);
+        match readline {
+            Ok(command) => {
+                repl.add_history_entry(command.as_str());
+                // println!("Line: {}", command);
+                if command.eq(".exit") {
+                    break;
+                } else {
+                    println!(
+                        "Error: unknown command or invalid arguments: '{}'. Enter '.help'",
+                        &command
+                    );
+                }
+            }
+            Err(ReadlineError::Interrupted) => {
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+        count += 1;
+    }
+    repl.append_history(".history.tmp").unwrap();
+    Ok(())
 }
---
+++ src/repl.rs
@@ -0,0 +1,94 @@
+use std::borrow::Cow::{self, Borrowed, Owned};
+
+use rustyline::config::OutputStreamType;
+use rustyline::error::ReadlineError;
+use rustyline::highlight::{Highlighter, MatchingBracketHighlighter};
+use rustyline::hint::{Hinter, HistoryHinter};
+use rustyline::validate::{MatchingBracketValidator, Validator};
+use rustyline::validate::{ValidationContext, ValidationResult};
+use rustyline::{CompletionType, Config, Context, EditMode};
+use rustyline_derive::{Completer, Helper};
+
+#[derive(Helper, Completer)]
+pub struct REPLHelper {
+    pub validator: MatchingBracketValidator,
+    pub colored_prompt: String,
+    pub hinter: HistoryHinter,
+    pub highlighter: MatchingBracketHighlighter,
+}
+
+impl REPLHelper {
+    pub fn new() -> Self {
+        REPLHelper {
+            highlighter: MatchingBracketHighlighter::new(),
+            hinter: HistoryHinter {},
+            colored_prompt: "".to_owned(),
+            validator: MatchingBracketValidator::new(),
+        }
+    }
+}
+
+impl Hinter for REPLHelper {
+    type Hint = String;
+
+    fn hint(&self, line: &str, pos: usize, ctx: &Context<'_>) -> Option<String> {
+        self.hinter.hint(line, pos, ctx)
+    }
+}
+
+impl Validator for REPLHelper {
+    fn validate(&self, ctx: &mut ValidationContext) -> Result<ValidationResult, ReadlineError> {
+        use ValidationResult::{Incomplete, /*Invalid,*/ Valid};
+        let input = ctx.input();
+        // let result = if !input.starts_with("SELECT") {
+        //     Invalid(Some(" --< Expect: SELECT stmt".to_owned()))
+        // } else
+        let result = if input.eq(".exit") {
+            Valid(None)
+        } else if !input.ends_with(';') {
+            Incomplete
+        } else {
+            Valid(None)
+        };
+        Ok(result)
+    }
+
+    fn validate_while_typing(&self) -> bool {
+        self.validator.validate_while_typing()
+    }
+}
+
+impl Highlighter for REPLHelper {
+    fn highlight_prompt<'b, 's: 'b, 'p: 'b>(
+        &'s self,
+        prompt: &'p str,
+        default: bool,
+    ) -> std::borrow::Cow<'b, str> {
+        if default {
+            Borrowed(&self.colored_prompt)
+        } else {
+            Borrowed(prompt)
+        }
+    }
+
+    fn highlight_hint<'h>(&self, hint: &'h str) -> Cow<'h, str> {
+        Owned("\x1b[1m".to_owned() + hint + "\x1b[m")
+    }
+
+    fn highlight<'l>(&self, line: &'l str, pos: usize) -> Cow<'l, str> {
+        self.highlighter.highlight(line, pos)
+    }
+
+    fn highlight_char(&self, line: &str, pos: usize) -> bool {
+        self.highlighter.highlight_char(line, pos)
+    }
+}
+
+pub fn get_config() -> Config {
+    Config::builder()
+        .history_ignore_space(true)
+        .completion_type(CompletionType::List)
+        .edit_mode(EditMode::Emacs)
+        .output_stream(OutputStreamType::Stdout)
+        .build()
+}
